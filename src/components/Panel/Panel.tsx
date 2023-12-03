72cb3a24f9b7acd332b93abd07cdad4eac104a8c
AJ Brush <19802883+ajb85@users.noreply.github.com>
2023-12-03 15:22:42 -0800

    Adds a small border radius to panels

--- src/components/Panel/Panel.tsx
+++ src/components/Panel/Panel.tsx
@@ -3,7 +3,12 @@ import { PanelProps } from "./types";
 
 export function Panel(props: PanelProps) {
   return (
-    <article className={combineClassNames("bg-panel-white", props.className)}>
+    <article
+      className={combineClassNames(
+        "bg-panel-white rounded-sm",
+        props.className
+      )}
+    >
       {props.children}
     </article>
   );
