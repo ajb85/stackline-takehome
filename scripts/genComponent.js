9da425f3d6b82aa55f92892d6f0e75881bcbb1ea
AJ Brush <19802883+ajb85@users.noreply.github.com>
2023-12-02 17:22:04 -0800

    Adds script to generate new component folders

---
+++ scripts/genComponent.js
@@ -0,0 +1,59 @@
+const fs = require("fs");
+const path = require("path");
+
+if (process.argv.length !== 3) {
+  console.error("Usage: npm run genComponent NAME");
+  process.exit(1);
+}
+
+const componentName = process.argv[2];
+const componentDir = path.join(
+  __dirname,
+  "..",
+  "src",
+  "components",
+  componentName
+);
+
+if (fs.existsSync(componentDir)) {
+  console.error(`Component ${componentName} already exists.`);
+  process.exit(1);
+}
+
+fs.mkdirSync(componentDir);
+
+const componentCode = `
+import { ${componentName}Props } from "./types";
+
+export function ${componentName}(props: ${componentName}Props) {
+  return <div>Hello World</div>;
+}
+`;
+
+fs.writeFileSync(
+  path.join(componentDir, `${componentName}.tsx`),
+  componentCode
+);
+
+const testCode = `
+import { ${componentName} } from "./${componentName}";
+
+it("renders", () => {
+  // TODO: test here
+});
+`;
+
+fs.writeFileSync(
+  path.join(componentDir, `${componentName}.test.tsx`),
+  testCode
+);
+
+fs.writeFileSync(path.join(componentDir, "util.ts"), "");
+
+const typesCode = `
+export type ${componentName}Props = {};
+`;
+
+fs.writeFileSync(path.join(componentDir, "types.ts"), typesCode);
+
+console.log(`Component ${componentName} generated successfully.`);
