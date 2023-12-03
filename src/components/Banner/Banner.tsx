808047af3f9fa38ace18a4ab09f65d433b816dbe
AJ Brush <19802883+ajb85@users.noreply.github.com>
2023-12-03 15:21:19 -0800

    Wraps the header in a div with an infinite blue background while the header (and its content) is limited to the appMaxWidth

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,11 +1,22 @@
 import { BannerProps } from "./types";
 import logo from "../../assets/stackline_logo.svg";
 import { logoAltText } from "./util";
+import { combineClassNames } from "../../util/combineClassNames";
+import { appMaxWidth, screenPadding } from "../../styling/sizing";
 
 export function Banner(props: BannerProps) {
   return (
-    <header className="bg-banner-blue p-4 mb-10">
-      <img className="w-[132px]" src={logo} alt={logoAltText} />
-    </header>
+    <div className="bg-banner-blue">
+      <header
+        className={combineClassNames(
+          "p-4 mb-10 mx-auto",
+          appMaxWidth,
+          screenPadding,
+          props.className
+        )}
+      >
+        <img className="w-[132px]" src={logo} alt={logoAltText} />
+      </header>
+    </div>
   );
 }
