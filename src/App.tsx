b7759e77f485cb079bc44891c9966994cb7d288a
AJ Brush <19802883+ajb85@users.noreply.github.com>
2023-12-03 18:17:25 -0800

    Renders product view under the banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Banner } from "./components/Banner/Banner";
+import { FetchCurrentProduct } from "./products/Fetch";
+import { ProductView } from "./products/View";
 
 function App() {
   return (
-    <div className="font-urbanist">
+    <div className="font-urbanist min-h-screen flex flex-col">
       <Banner />
-      <h2>Hello World</h2>
+
+      <FetchCurrentProduct>
+        <ProductView />
+      </FetchCurrentProduct>
     </div>
   );
 }
