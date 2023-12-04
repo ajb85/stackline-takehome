df785f2249834b2e7123c9b64fe7f79d3aab16ea
AJ Brush <19802883+ajb85@users.noreply.github.com>
2023-12-03 17:28:56 -0800

    Removes unused import

--- src/products/Fetch.tsx
+++ src/products/Fetch.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { getSearchParams } from "../util/getSearchParams";
 import { FetchProductProps } from "./types";
 import { useProducts, fetchProductById } from "./reducer";
