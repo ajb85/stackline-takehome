050dac046351264ff34a3becc5416be52a58e1d1
AJ Brush <19802883+ajb85@users.noreply.github.com>
2023-12-03 21:23:32 -0800

    Wraps app test in a provider

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 import { logoAltText } from "./components/Banner/util";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
 test("Renders the logo", () => {
-  render(<App />);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
   screen.getByAltText(logoAltText);
 });
